9e1a3fcf7882a3962de16a2c35e364a7d5e24713
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-04-03 13:16:58 +0200

    ♻️ refactor: Component styles scoped

--- src/components/homepage/SectionOne.js
+++ src/components/homepage/SectionOne.js
@@ -1,5 +1,5 @@
 import { Box, Typography, Button } from "@mui/material"
-import "../../styles/components/homepage/sectionOne.scss"
+import styles from "../../styles/components/homepage/sectionOne.module.scss"
 import illustration from "../../assets/homepage/undraw_in_progress_re_m1l6.svg"
 import waves from "../../assets/homepage/waves.svg"
 
@@ -7,33 +7,33 @@ export default function SectionOne() {
     
     return(
         <>
-            <Box id="sectionOne">
-                <Typography id="typoLogo" variant="h3" color="initial">Développement Personnel</Typography>
-                <Box id="subSection">
-                    <Box id="subSectionLeft">
-                        <Box id="boxTitle">
+            <Box id={styles.sectionOne}>
+                <Typography id={styles.typoLogo} variant="h3" color="initial">Développement Personnel</Typography>
+                <Box id={styles.subSection}>
+                    <Box id={styles.subSectionLeft}>
+                        <Box id={styles.boxTitle}>
                             <Typography variant="h1" color="initial">Lorem Ipsum Dolor Sit Amet</Typography>
                         </Box>
-                        <Box id="boxSubtitle">
+                        <Box id={styles.boxSubtitle}>
                             <Typography variant="h2" color="initial">Lorem Ipsum Dolor Sit Amet</Typography>
 
                         </Box>
-                        <Box id="boxButton">
-                            <Box id="boxButton2">
-                                <Button className="containedButton button" variant="contained" color="primary">
+                        <Box id={styles.boxButton}>
+                            <Box id={styles.boxButton2}>
+                                <Button className={styles.containedButton} variant="contained" color="primary">
                                     Commencer
                                 </Button>
-                                <Button className="outlinedButton button" variant="outlined" color="primary">
+                                <Button className={styles.outlinedButton} variant="outlined" color="primary">
                                     J'ai déjà un compte
                                 </Button>
                             </Box>
                         </Box>
                     </Box>
-                    <Box id="subSectionRight">
-                        <img className="illustration" alt="Illustration d'une progression" src={illustration} />
+                    <Box id={styles.subSectionRight}>
+                        <img className={styles.illustration} alt="Illustration d'une progression" src={illustration} />
                     </Box>
                 </Box>
-                <img id="waves" alt="Vagues" src={waves} />
+                <img id={styles.waves} alt="Vagues" src={waves} />
             </Box>
         
         </>
--- src/components/homepage/sectionTwo.js
+++ src/components/homepage/sectionTwo.js
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material"
-import "../../styles/components/homepage/sectionTwo.scss"
+import styles from "../../styles/components/homepage/sectionTwo.module.scss"
 import illustration1 from "../../assets/homepage/undraw_questions_re_1fy7.svg"
 import illustration2 from "../../assets/homepage/undraw_percentages_re_a1ao.svg"
 
@@ -8,20 +8,20 @@ export default function SectionTwo() {
     
     return(
         <>
-            <Box id="sectionTwo">
-                <Box id="boxTitle">
-                    <Typography id="title" variant="h2" color="initial">Comment ça fonctionne ?</Typography>
+            <Box id={styles.sectionTwo}>
+                <Box id={styles.boxTitle}>
+                    <Typography id={styles.title} variant="h2" color="initial">Comment ça fonctionne ?</Typography>
                 </Box>
-                <Box id="boxSubSection">
-                    <Box id="boxSubsection1">
+                <Box id={styles.boxSubSection}>
+                    <Box id={styles.boxSubsection1}>
                         <Typography variant="h4" color="initial">Vous devez répondre à 20 questions pour déterminer votre niveau de développement personnel.</Typography>
-                        <img className="illustration1" alt="Illustration d'une progression" src={illustration1} />
+                        <img className={styles.illustration1} alt="Illustration d'une progression" src={illustration1} />
                     </Box>
-                    <Box id="boxSubsection2">
+                    <Box id={styles.boxSubsection2}>
                         <Typography variant="h4" color="initial">Vous obtenez un résultat sous forme de pourcentage pour chaque aspect du développement personnel.</Typography>
-                        <img className="illustration2" alt="Illustration d'une progression" src={illustration2} />
+                        <img className={styles.illustration2} alt="Illustration d'une progression" src={illustration2} />
                     </Box>
-                    <Typography id="typoResult" variant="h3" color="initial">Vous pourrez ensuite accéder à une sélection d'articles et d'astuces pour vous !</Typography>
+                    <Typography id={styles.typoResult} variant="h3" color="initial">Vous pourrez ensuite accéder à une sélection d'articles et d'astuces pour vous !</Typography>
                 </Box>
             </Box>
         </>
