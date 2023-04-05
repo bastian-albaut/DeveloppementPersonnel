80bfb766ea44acfbf80491a9b15cba081c7ea31d
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-04-05 08:51:59 +0200

    refactor : Sample of data for Quiz

---
+++ src/assets/quiz/listQuestions.js
@@ -0,0 +1,186 @@
+const quiz = {
+    questions: [
+      {
+        question: "What is the capital of France?",
+        answers: [
+          { text: "London", correct: false },
+          { text: "Paris", correct: true },
+          { text: "Berlin", correct: false },
+          { text: "Madrid", correct: false }
+        ]
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+          { text: "Jupiter", correct: true },
+          { text: "Mars", correct: false },
+          { text: "Venus", correct: false },
+          { text: "Neptune", correct: false }
+        ]
+      },
+      {
+        question: "Who wrote the novel 'To Kill a Mockingbird'?",
+        answers: [
+          { text: "Harper Lee", correct: true },
+          { text: "F. Scott Fitzgerald", correct: false },
+          { text: "William Faulkner", correct: false },
+          { text: "Ernest Hemingway", correct: false }
+        ]
+      },
+      {
+        question: "What is the capital of France?",
+        answers: [
+          { text: "London", correct: false },
+          { text: "Paris", correct: true },
+          { text: "Berlin", correct: false },
+          { text: "Madrid", correct: false }
+        ]
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+          { text: "Jupiter", correct: true },
+          { text: "Mars", correct: false },
+          { text: "Venus", correct: false },
+          { text: "Neptune", correct: false }
+        ]
+      },
+      {
+        question: "Who wrote the novel 'To Kill a Mockingbird'?",
+        answers: [
+          { text: "Harper Lee", correct: true },
+          { text: "F. Scott Fitzgerald", correct: false },
+          { text: "William Faulkner", correct: false },
+          { text: "Ernest Hemingway", correct: false }
+        ]
+      },
+      {
+        question: "What is the capital of France?",
+        answers: [
+          { text: "London", correct: false },
+          { text: "Paris", correct: true },
+          { text: "Berlin", correct: false },
+          { text: "Madrid", correct: false }
+        ]
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+          { text: "Jupiter", correct: true },
+          { text: "Mars", correct: false },
+          { text: "Venus", correct: false },
+          { text: "Neptune", correct: false }
+        ]
+      },
+      {
+        question: "Who wrote the novel 'To Kill a Mockingbird'?",
+        answers: [
+          { text: "Harper Lee", correct: true },
+          { text: "F. Scott Fitzgerald", correct: false },
+          { text: "William Faulkner", correct: false },
+          { text: "Ernest Hemingway", correct: false }
+        ]
+      },
+      {
+        question: "What is the capital of France?",
+        answers: [
+          { text: "London", correct: false },
+          { text: "Paris", correct: true },
+          { text: "Berlin", correct: false },
+          { text: "Madrid", correct: false }
+        ]
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+          { text: "Jupiter", correct: true },
+          { text: "Mars", correct: false },
+          { text: "Venus", correct: false },
+          { text: "Neptune", correct: false }
+        ]
+      },
+      {
+        question: "Who wrote the novel 'To Kill a Mockingbird'?",
+        answers: [
+          { text: "Harper Lee", correct: true },
+          { text: "F. Scott Fitzgerald", correct: false },
+          { text: "William Faulkner", correct: false },
+          { text: "Ernest Hemingway", correct: false }
+        ]
+      },
+      {
+        question: "What is the capital of France?",
+        answers: [
+          { text: "London", correct: false },
+          { text: "Paris", correct: true },
+          { text: "Berlin", correct: false },
+          { text: "Madrid", correct: false }
+        ]
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+          { text: "Jupiter", correct: true },
+          { text: "Mars", correct: false },
+          { text: "Venus", correct: false },
+          { text: "Neptune", correct: false }
+        ]
+      },
+      {
+        question: "Who wrote the novel 'To Kill a Mockingbird'?",
+        answers: [
+          { text: "Harper Lee", correct: true },
+          { text: "F. Scott Fitzgerald", correct: false },
+          { text: "William Faulkner", correct: false },
+          { text: "Ernest Hemingway", correct: false }
+        ]
+      },
+      {
+        question: "What is the capital of France?",
+        answers: [
+          { text: "London", correct: false },
+          { text: "Paris", correct: true },
+          { text: "Berlin", correct: false },
+          { text: "Madrid", correct: false }
+        ]
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+          { text: "Jupiter", correct: true },
+          { text: "Mars", correct: false },
+          { text: "Venus", correct: false },
+          { text: "Neptune", correct: false }
+        ]
+      },
+      {
+        question: "Who wrote the novel 'To Kill a Mockingbird'?",
+        answers: [
+          { text: "Harper Lee", correct: true },
+          { text: "F. Scott Fitzgerald", correct: false },
+          { text: "William Faulkner", correct: false },
+          { text: "Ernest Hemingway", correct: false }
+        ]
+      },
+      {
+        question: "What is the capital of France?",
+        answers: [
+          { text: "London", correct: false },
+          { text: "Paris", correct: true },
+          { text: "Berlin", correct: false },
+          { text: "Madrid", correct: false }
+        ]
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+          { text: "Jupiter", correct: true },
+          { text: "Mars", correct: false },
+          { text: "Venus", correct: false },
+          { text: "Neptune", correct: false }
+        ]
+      },
+    ]
+  };
+
+export default quiz;
