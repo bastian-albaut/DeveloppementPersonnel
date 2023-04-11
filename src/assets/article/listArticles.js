b034a61eaf3977072b012e2fc81b0099f63cf624
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-04-11 15:49:59 +0200

    refactor: Update sample data

---
+++ src/assets/article/listArticles.js
@@ -0,0 +1,64 @@
+const articles = [
+    {
+        title: 'The Benefits of Yoga',
+        description: 'Discover the physical and mental benefits of a regular yoga practice',
+        picture: 'https://picsum.photos/500/300',
+        date: '2022-03-15',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        categorie_id: '123',
+        categorie_name: 'Health and Fitness',
+        professional_id: '456',
+        professional_firstname: 'John',
+        professional_lastname: 'Doe'
+    },
+    {
+        title: 'The Importance of Sleep',
+        description: 'Learn about the role of sleep in maintaining overall health and wellbeing',
+        picture: 'https://picsum.photos/500/300',
+        date: '2022-03-18',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        categorie_id: '123',
+        categorie_name: 'Health and Fitness',
+        professional_id: '789',
+        professional_firstname: 'Jane',
+        professional_lastname: 'Smith'
+    },
+    {
+        title: 'The Benefits of Meditation',
+        description: 'Explore the mental and emotional benefits of a regular meditation practice',
+        picture: 'https://picsum.photos/500/300',
+        date: '2022-04-01',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        categorie_id: '456',
+        categorie_name: 'Mental Health',
+        professional_id: '123',
+        professional_firstname: 'Sarah',
+        professional_lastname: 'Johnson'
+    },
+    {
+        title: '10 Healthy Eating Habits to Adopt Now',
+        description: 'Discover simple but effective tips for eating a balanced and nutritious diet',
+        picture: 'https://picsum.photos/500/300',
+        date: '2022-04-05',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        categorie_id: '789',
+        categorie_name: 'Nutrition',
+        professional_id: '456',
+        professional_firstname: 'David',
+        professional_lastname: 'Lee'
+    },
+    {
+        title: 'How to Stay Motivated for Your Workouts',
+        description: 'Learn strategies for staying motivated and consistent with your fitness routine',
+        picture: 'https://picsum.photos/500/300',
+        date: '2022-04-08',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+        categorie_id: '123',
+        categorie_name: 'Health and Fitness',
+        professional_id: '789',
+        professional_firstname: 'Emily',
+        professional_lastname: 'Wang'
+    }
+];
+
+export default articles;
