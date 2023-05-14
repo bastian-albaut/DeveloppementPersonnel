e21aea60c666340c554b41b46427f7b96cb6ff47
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-05-14 15:25:20 +0200

    💄 style: Adjust style dashboard

--- src/components/dashboard/sectionQuote.js
+++ src/components/dashboard/sectionQuote.js
@@ -15,7 +15,7 @@ export default function SectionQuote() {
 
     return(
         <Box id={styles.boxSection}>
-            <Typography id={styles.typoTitle} variant="h1" color="initial">Citation du jour</Typography>
+            <Typography id={styles.typoTitle} variant="h3" color="initial">Citation du jour</Typography>
             <Typography id={styles.typoQuote} variant="caption" color="initial">{quote.content}</Typography>
             <Box id={styles.boxAuthor}>
                 <Typography id={styles.typoAuthor} variant="body1" color="initial">{quote.author}</Typography>
