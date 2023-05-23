1574ff9e659a3820389343e05111db2cdd697195
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-05-23 23:39:43 +0200

    📱 feat: Adjust responsive design result page

--- src/components/quizResult/CircularProgressWithLabel.js
+++ src/components/quizResult/CircularProgressWithLabel.js
@@ -1,17 +1,22 @@
-import { Box, CircularProgress, Typography } from "@mui/material";
+import { Box, CircularProgress, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
 
 import styles from "../../styles/components/quizResult/circularProgressWithLabel.module.scss"
 
 export default function CircularProgressWithLabel(props) {
-  return (
-    <Box id={styles.boxCircularProgress}>
-      <CircularProgress id={styles.circularProgress} variant="determinate" {...props} size={100}/>
-      <Box id={styles.boxTextCircularProgress}>
-        <Typography id={styles.typoTextCircularProgress} variant="caption" component="div" color="text.primary">
-          {`${Math.round(props.value)}%`}
-        </Typography>
-      </Box>
-    </Box>
-  );
+
+    const isSmallScreen = useMediaQuery('(max-width: 600px)');
+
+    const circularProgressSize = isSmallScreen ? 70 : 100; // Define the size based on the media query
+
+    return (
+        <Box id={styles.boxCircularProgress}>
+            <CircularProgress id={styles.circularProgress} variant="determinate" {...props} size={circularProgressSize}/>
+                <Box id={styles.boxTextCircularProgress}>
+                    <Typography id={styles.typoTextCircularProgress} variant="caption" component="div" color="text.primary">
+                    {`${Math.round(props.value)}%`}
+                    </Typography>
+            </Box>
+        </Box>
+    );
 }
