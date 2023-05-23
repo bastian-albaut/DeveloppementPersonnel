6679023036afc53343025ffe605df73fd48e8804
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-05-23 23:20:18 +0200

    ✨ feat: Display correct informations on card result page

--- src/components/quizResult/CardResult.js
+++ src/components/quizResult/CardResult.js
@@ -5,13 +5,14 @@ import styles from "../../styles/components/quizResult/cardResult.module.scss"
 import CircularProgressWithLabel from './CircularProgressWithLabel.js';
 
 export default function CardResult(props) {
+    // console.log(props.score.category_name)
     return (
         <Card key={props.index} id={styles.cardResult}>
             <Box id={styles.boxSubResult}>
                 <CircularProgressWithLabel value={props.score.percentage}/>
-                <Typography id={styles.typoCategorieName} variant="h3" color="text.primary">{props.score.categorie_name}</Typography>
+                <Typography id={styles.typoCategorieName} variant="h3" color="text.primary">{props.score.category_name}</Typography>
             </Box>
-            <Typography id={styles.typoCategorieDescription} variant="body1" color="text.primary">{props.description}</Typography>
+            <Typography id={styles.typoCategorieDescription} variant="body1" color="text.primary">{props.score.description}</Typography>
         </Card>
     )
 }
--- src/components/quizResult/SectionQuizResult.js
+++ src/components/quizResult/SectionQuizResult.js
@@ -2,23 +2,23 @@ import React from 'react';
 import Typography from '@mui/material/Typography'
 import { Box } from '@mui/material';
 
-import result from '../../assets/result/listResult';
 import descriptions from '../../assets/result/listDescription';
 
 import CardResult from './CardResult';
 
 import styles from "../../styles/components/quizResult/sectionQuizResult.module.scss"
 
-export default function SectionQuizResult() {
-
+export default function SectionQuizResult(props) {
+    console.log("props.result")
+    console.log(props.result)
     return(
         <Box id={styles.boxSection}>
             <Typography id={styles.typoTitle} variant="h1" color="initial">Résultat</Typography>
             <Typography id={styles.typoSubTitle} variant="subtitle1" color="initial">Estimation de votre niveau pour tous les apects du développement personnel.</Typography>
             <Box id={styles.boxSubSection}>
-                {result.scores.map((score, index) => {
+                {props.result.score.map((score, index) => {
                     return (
-                        <CardResult key={index} score={score} description={descriptions[index]}/>
+                        <CardResult key={index} score={score}/>
                     )
                 })}
             </Box>
